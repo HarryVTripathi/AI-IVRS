4c78c5632c4ef99f98cd5e68f00029507f4cd1b4
earljohannes <helmuthziggler@gmail.com>
2020-01-26 17:12:47 +0530

    added audio save

--- src/middleware/tts.js
+++ src/middleware/tts.js
@@ -1,17 +1,22 @@
+const fs = require('fs');
 const request = require('request');
 const { Router } = require('express');
 
 const router = Router();
 
 async function getSpeech(req, res, next) {
+  console.info('Request received, generating speech...');
   const url = 'https://texttospeech.googleapis.com/v1beta1/text:synthesize?key=AIzaSyCGNPwkrjEqEJwGdo8T0oQAhihhOCfqDic';
+
+  const { body } = req;
+  console.log(body);
 
   const headers = {
     'Content-Type': "application/json",
     'Authentication': "ya29.c.Kmq6BxOjtxVSMa04lRr_SgO_Sv8a19ka2WmyThNeonRMHZZuPs15kg1Sg1nV-jp9xJlUZ68UUrP9nnEDAOJNYMcPeXpSvKKLZ19p1GsU9YHCZFFTeeUO1auFolxUak9MYpBeesjNHQ1eUrKP"
   };
   
-  const body = {
+  const body_legacy = {
     input: {
       text:"Android is a mobile operating system developed by Google, based on the Linux kernel and designed primarily for touchscreen mobile devices such as smartphones and tablets."
     },
@@ -33,14 +38,20 @@ async function getSpeech(req, res, next) {
   };
 
   try {
-    const { body: { audioContent } } = await fun(options);
-    res.status(200).json({ base64Audio: audioContent});
+    const { body: { audioContent: base64Data } } = await makeRequest(options);
+    const TTSResult = base64Data ? "SUCCESS": "FAILURE";
+    fs.writeFileSync('file.mp3', Buffer.from(base64Data.replace('data:audio/mp3; codecs=opus;base64,', ''), 'base64'));
+
+    console.info('Completed...')
+    res.status(200).json({ TTSResult });
+
   } catch (error) {
-    res.status(error.status).json({ err: "error"});
+    console.log(error);
+    res.status(500).json({ err: "error in tts"});
   }
 }
 
-function fun (options) {
+function makeRequest (options) {
   return new Promise((resolve, reject) => {
     request(options, (error, response) => {
       if (error)
@@ -51,13 +62,7 @@ function fun (options) {
   })
 }
 
-router.get('/', getSpeech);
-// fun(options)
-//   .then((res) => {
-//     console.log(res);
-//   })
-//   .catch((error) => {
-//     console.log(error);
-//   });
+router.route('/')
+  .get(getSpeech);
 
 module.exports = { ttsRouter: router };

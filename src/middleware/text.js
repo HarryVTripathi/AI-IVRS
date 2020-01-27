8771c97971d45ebe9a6afb703cc707850b5ec05e
earljohannes <helmuthziggler@gmail.com>
2020-01-28 01:34:02 +0530

    added head

--- src/middleware/text.js
+++ src/middleware/text.js
@@ -4,11 +4,16 @@ const { Router } = require('express');
 const router = Router();
 
 function readText(req, res, next) {
-  const data = fs.readFileSync('../../a.txt', { encoding: 'utf-8' });
+  console.info('Reading text from url...');
+  console.log(req.method);
+  res.set('Content-Type', 'text/plain');
+  const data = fs.readFileSync('./a.txt', { encoding: 'utf-8' });
   res.status(200).send(data);
 }
 
 router.get('/', readText);
+
+router.head('/hd', readText);
 
 module.exports = {
   textRouter: router,
